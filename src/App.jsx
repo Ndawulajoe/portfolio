835378d1fd3158ad69d227d934f8eac6240ae14d
NdawulaJoe <ndawulajoseph255@gmail.com>
2023-08-22 13:32:03 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Home from './components/Home'
+import MyHeader from './components/MyHeader'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+  return (
+   <>
+   <MyHeader/>
+   <Home/>
+    <Footer/>
+   </>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,114 @@
+import { createStyles, Container, Title, Text, Button, rem } from '@mantine/core';
+import myImg from '../My-img.jpeg';
+const useStyles = createStyles((theme) => ({
+  root: {
+    // backgroundColor: '#11284b',
+    backgroundSize: 'cover',
+    // backgroundPosition: '',
+    backgroundImage: `url(${myImg})`,
+    paddingTop: `calc(${theme.spacing.xl} * 6)`,
+    paddingBottom: `calc(${theme.spacing.xl} * 6)`,
+  },
+
+  inner: {
+    display: 'flex',
+    justifyContent: 'space-between',
+
+    [theme.fn.smallerThan('md')]: {
+      flexDirection: 'column',
+    },
+  },
+
+  image: {
+    [theme.fn.smallerThan('md')]: {
+      display: 'none',
+    },
+  },
+
+  content: {
+    paddingTop: `calc(${theme.spacing.xl} * 2)`,
+    paddingBottom: `calc(${theme.spacing.xl} * 2)`,
+    marginRight: `calc(${theme.spacing.xl} * 3)`,
+
+    [theme.fn.smallerThan('md')]: {
+      marginRight: 0,
+    },
+  },
+
+  title: {
+    color: theme.white,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontWeight: 900,
+    lineHeight: 1.05,
+    maxWidth: rem(500),
+    fontSize: rem(48),
+
+    [theme.fn.smallerThan('md')]: {
+      maxWidth: '100%',
+      fontSize: rem(34),
+      lineHeight: 1.15,
+    },
+  },
+
+  description: {
+    color: theme.white,
+    opacity: 0.75,
+    maxWidth: rem(500),
+
+    [theme.fn.smallerThan('md')]: {
+      maxWidth: '100%',
+    },
+  },
+
+  control: {
+    paddingLeft: rem(50),
+    paddingRight: rem(50),
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontSize: rem(22),
+
+    [theme.fn.smallerThan('md')]: {
+      width: '100%',
+    },
+  },
+}));
+
+export default function Home() {
+  const { classes } = useStyles();
+  return (
+    <div className={classes.root}>
+      <Container size="lg">
+        <div className={classes.inner}>
+          <div className={classes.content}>
+            <Title className={classes.title}>
+             Am{' '}
+              <Text
+                component="span"
+                inherit
+                variant="gradient"
+                gradient={{ from: 'pink', to: 'yellow' }}
+              >
+               Joseph
+              </Text>{' '}
+               Wampamba
+            </Title>
+
+            <Text className={classes.description} mt={30}>
+              Build fully functional accessible web applications with ease – Mantine includes more
+              than 100 customizable components and hooks to cover you in any situation
+            </Text>
+
+            <Button
+              variant="gradient"
+              gradient={{ from: 'pink', to: 'yellow' }}
+              size="xl"
+              className={classes.control}
+              mt={40}
+            >
+              Get started
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+}
